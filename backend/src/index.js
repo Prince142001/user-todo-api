5a1c4cdac44995ec438764865cf0b54f53620a39
Prince142001 <prince.14.2000@gmail.com>
2025-12-03 19:29:06 +0530

    Done with the database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
 import express from "express";
+import connectDB from "./db/index.js";
+
+import dotenv from "dotenv";
 
 const app = express();
-
-// app.get("/", (req, res)=>{
-//     res.send()
-// })
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server started on the port ${PORT}`));
+dotenv.config({
+    path: "./env",
+});
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started on the port ${PORT}`);
+        });
+        app.on("error", (error) => {
+            console.log("Error", error);
+            throw error;
+        });
+    })
+    .catch((error) => {
+        console.error("ERROR", error);
+        throw error;
+    });
